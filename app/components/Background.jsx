4a268f776223248c21433c46d7915408a25ef942
Pedro <50713178+prgf87@users.noreply.github.com>
2023-10-03 16:47:01 +0100

    bg animation

--- app/components/Background.jsx
+++ app/components/Background.jsx
@@ -6,22 +6,25 @@ export default function Background() {
     <motion.div
       initial={{
         opacity: 0,
+        scale: 1,
       }}
       animate={{
         scale: [1, 2, 2, 3, 1],
-        opacity: [0.1, 0.2, 0.4, 0.8, 0.4, 0.1, 1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1],
+        borderRadius: ['20%', '20%', '50%', '80%', '20%'],
       }}
       transition={{
         duration: 2.5,
+        // repeat: Infinity,
       }}
       className="relative flex justify-center items-center"
     >
-      <div className="border border-slate-500 rounded-full h-[200px] w-[200px] animate-ping" />
-      <div className="border border-slate-500 rounded-full h-[300px] w-[300px] absolute" />
-      <div className="border border-slate-500 rounded-full h-[500px] w-[500px] absolute" />
+      <div className="border border-slate-500 rounded-full h-[200px] w-[200px] animate-ping mt-52" />
+      <div className="border border-slate-500 rounded-full h-[300px] w-[300px] absolute mt-52 animate-pulse" />
+      <div className="border border-slate-500 rounded-full h-[500px] w-[500px] absolute mt-52 animate-pulse" />
 
-      <div className="border border-slate-200 rounded-full opacity-20 h-[650px] w-[650px] animate-pulse absolute" />
-      <div className="border border-slate-500 rounded-full h-[800px] w-[800px] absolute" />
+      <div className="border border-slate-200 rounded-full opacity-20 h-[650px] w-[650px] animate-pulse duration-500 absolute mt-52" />
+      <div className="border border-slate-500 rounded-full h-[800px] w-[800px] absolute mt-52 animate-pulse duration-500" />
       <div />
     </motion.div>
   );
--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -2,13 +2,15 @@
 import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import Background from './Background';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 export default function Hero() {
   const [text, count] = useTypewriter({
     words: [
       "Hi, my name's Pedro Ferreira",
-      'coffee-drinker.jsx',
-      '<WithAPurpose />',
+      'code-crafter.jsx',
+      '<SoftwareEngineer />',
     ],
     loop: 0,
     delaySpeed: 2500,
@@ -16,10 +18,24 @@ export default function Hero() {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <Background />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#eee" />
-      </h1>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: [0, 0, 0, 0.8, 1] }}
+        transition={{ duration: 5 }}
+        className="absolute"
+      >
+        <Image
+          src={'/profile-pic.jpg'}
+          alt="/"
+          width={800}
+          height={800}
+          className="rounded-full relative h-32 w-32 mx-auto border-2 border-white/20"
+        />
+        <h1>
+          <span>{text}</span>
+          <Cursor cursorColor="#eee" />
+        </h1>
+      </motion.div>
     </div>
   );
 }
