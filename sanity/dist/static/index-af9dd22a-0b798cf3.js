539846f7bea46acbcb901e9e65bd6f224186fd30
Pedro <50713178+prgf87@users.noreply.github.com>
2023-10-07 20:05:31 +0100

    1.0 build

--- sanity/dist/static/index-af9dd22a-0b798cf3.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPane as t}from"./index-ccbd3c57-0e49fddd.js";import{D as m}from"./sanity-a86e3db8.js";import"./json-inspector-79ce3dec.js";export{m as DocumentPaneProvider,t as default};
---
+++ sanity/dist/static/index-af9dd22a-ab13b347.js
@@ -0,0 +1,1 @@
+import{DocumentPane as t}from"./index-ccbd3c57-74a6b08a.js";import{D as m}from"./sanity-b5b9075a.js";import"./json-inspector-48072f3d.js";export{m as DocumentPaneProvider,t as default};
