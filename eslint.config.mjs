aafb47b70dab062819d3d5cc05faa36664fc696b
Pedro Ferreira <prgf2011@gmail.com>
2026-02-06 13:14:45 +0000

    updated nextjs for safety reasons and fixes other vunerabilites/dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+export default [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    ignores: ["node_modules/**", ".next/**"],
+  },
+];
